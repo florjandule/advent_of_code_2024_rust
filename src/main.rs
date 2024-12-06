5a7204deca6140c428fec016cf9db45a2df61502
Dule, Florjan <florjan.dule@zoi.tech>
2024-12-06 23:04:21 +0100

    add execution time

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use advent_of_code_2024_rust::*;
 
 fn main() {
+
+    let time = std::time::Instant::now();
     day1::execute();
+    let time = time.elapsed();
+    println!("Execution Time: {} us.", time.as_micros());
 }
